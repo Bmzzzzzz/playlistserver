31ea8025e1dc84c762dc3e90fd3e935f59fe7566
ariel gold <ariel6185@gmail.com>
2022-07-07 19:45:31 +0300

    forgot add a file

---
+++ middlware/jwt.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken")
+const secret = process.env.SECRET_JWT
+
+
+function createToken(id) {
+    const token = jwt.sign({ id }, secret, { expiresIn: "1m" })
+    return token
+}
+
+function validateToken(token) {
+    return jwt.verify(token, secret)
+}
+
+module.exports = { createToken, validateToken }
